ce35e37607210755739ca9aca0a2bf560f062c2d
ChoiSeungWoo960624 <hufs0100745@gmail.com>
2025-01-03 17:00:37 +0900

    연결확인용

---
+++ 1.py
@@ -0,0 +1,39 @@
+1   #한 줄 주석
+'''
+여기는
+여러줄
+주석입니다.
+'''
+
+"""
+쌍따옴표 새개도
+여러줄
+주석입니다.
+"""
+
+# (ctr1 + /) //주석 한줄 짜리
+
+
+'''print ("hello world")
+print ("hello", "world")
+print ("hello")
+print ("hello", "word", sep="")     #// seperate, 구분자
+print ("010", "8280", "0745", sep="-")
+print ("hello", "python", 1, 2, sep="_")    #//자료형 달라도 됨.
+print ()    #// print 함수 안에 아무것도 안넣으면 줄 바꿈 처리가 됨.
+print ("1111")
+print ("안녕하세요", end="")
+print ("코딩온 입니다.")
+
+print ()
+print ("안녕하세요", end=", ")
+print ("코딩온 입니다.")'''
+
+"""ive = "I AM"
+print (ive)
+ive = "장원영"
+print (ive)
+print (f"제가 좋아하는 가수는 {ive}입니다.")    #//f 문자열 포매팅
+print ("제가 좋아하는 가수는", ive, "입니다.", sep="")
+#// 디버깅 코드 ctrl + f5 """
+
