b73aebd082b4a08e98e22ac131736c2ae063db23
ChoiSeungWoo960624 <hufs0100745@gmail.com>
2025-01-06 20:49:22 +0900

    내장함수 추가

--- 8.py
+++ 8.py
@@ -54,3 +54,31 @@ print(fruits.items()) # dict_items([('apple', '사과'), ('banana', '바 나나'
 fruits.clear()
 print(fruits) #{}
 
+#성적관리
+fruits = {"Alice": "85", "Bob": "90", "Charlie": "95"}
+fruits["David"] = "80"
+fruits["Alice"] = 88
+del fruits["Bob"]
+print(fruits)
+
+# 내장 함수
+numbers = [10, 20, 30, 40]
+print(sum(numbers))
+
+scores = {"국어": 83, "영어": 90, "수학": 95}
+print(scores.values())
+total_score = sum(scores.values())
+print(total_score)
+
+print(max(numbers))
+print(min(numbers))
+
+print(max(scores.values()))
+print(min(scores.values()))
+
+names = ["Alice", "Bob", "Charlie", "Lily"]
+scores = [85, 90, 95, 100, 105]
+zipped = list(zip(names, scores))
+zipped2 = dict(zip(names, scores))
+print(zipped)
+print(zipped2)
