fe22318a032be6891046df13b86a43d6bfaafac4
ChoiSeungWoo960624 <hufs0100745@gmail.com>
2025-01-08 16:19:14 +0900

    25.1.8. 이중for문

---
+++ 과제 자판기.py
@@ -0,0 +1,12 @@
+vending_machine = ['게토레이', '게토레이', '레스비', '레스비', '생수', '생수', '생수', '이프로']
+
+{
+host = input("할 일 선택: ")
+
+{gast = input("마시고 싶은 음료? ") # 조건2
+if vending_machine in [f"{vending_machine}"]:
+    print(f"{gast} 드릴게요")
+else:
+    print("현재 해당물품은 제고가 다 떨어졌습니다.")
+}
+}
